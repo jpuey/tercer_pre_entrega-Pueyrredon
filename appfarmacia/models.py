599317545c2b6e1d3f087967dd3ca10622f87523
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-07-28 16:13:46 -0300

    Virtual Enviroment, Proyecto y App

---
+++ appfarmacia/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Medicamento(models.Model):
+    nombre= models.CharField(max_length=20)
+    droga= models.CharField(max_length=20)
+    inventario= models.IntegerField()
+    receta= models.BooleanField()
+
+
+class Medico(models.Model):
+    nombre= models.CharField(max_length=10)
+    apellido= models.CharField(max_length=10)
+    especialidad= models.CharField (max_length=20)
+    matricula= models.IntegerField ()
+    email= models.EmailField ()
+
+
+class Paciente(models.Model):
+    nombre= models.CharField(max_length=10)
+    apellido= models.CharField(max_length=10)
+    obra_social= models.CharField(max_length=20)
+    nafiliado= models.IntegerField()
